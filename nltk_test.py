9804e25a7535abc7a8146eb1ac15a4b07d8a4811
Andrew <andrew@upwords.co>
2016-02-28 17:56:37 -0800

    Build cfg randomly

--- nltk_test.py
+++ nltk_test.py
@@ -1,57 +1,50 @@
 import nltk
-from nltk.parse import RecursiveDescentParser
-from nltk.parse.generate import generate, demo_grammar
+import random
+from nltk.parse.generate import generate
 from nltk import CFG
-
 
 titles = ["14 ways to derp a best friend", "5 ways to shave a cat"] 
 
-processed_titles = []
-
+#tokenize
+tokenized_titles = []
 for title in titles:
     tokenized = nltk.word_tokenize(title)
-    tagged = nltk.pos_tag(tokenized)
-    processed_titles.append(tagged)
+    tokenized_titles.append(tokenized)
+
+#tag
+tagged_titles = []
+for title in tokenized_titles:
+    tagged = nltk.pos_tag(title)
+    tagged_titles.append(tagged)
 
 #flatten lists of tagged words
-all_tags = [item for sublist in processed_titles for item in sublist]
+all_tags = [item for sublist in tagged_titles for item in sublist]
 
+#Build tag word map
 tag_word_map = {}
-
 for word, tag in all_tags:
     if tag in tag_word_map:
         tag_word_map[tag].append(word)   
     else:
         tag_word_map[tag] = [word]
 
-cfg = "S -> NP VP\n"
-cfg += "NP -> VB\n" 
-cfg += "VP -> NN\n" 
+sentence_structures = []
+for word_tag_pair in tagged_titles:
+    temp = []
+    for pair in word_tag_pair:
+        temp.append(pair[1])
+    sentence_structures.append(temp)
 
+start_tag_list = random.choice(sentence_structures)
+start = str.join(" ", start_tag_list)
 
-demo_grammar = """
-    S -> NP VP
-    NP -> 'derp'
-    VP -> 'cat'
-    """
+#Build CFG
+cfg = "S -> " + start + "\n"
+for tag in start_tag_list:
+    terminal = random.choice(tag_word_map[tag])
+    cfg += tag + " -> " + "'" + terminal + "'" + "\n"
 
-for tag, words in tag_word_map.iteritems():
-    cfg += tag + ' -> '  
-    words = list(set(words))
-    for word in words:
-        if word != words[-1]: 
-            cfg += "'" + word + "'" + ' | '
-        else:
-            cfg += "'" + word + "'"
-    cfg += "\n"
-
-print cfg
-grammar1 = nltk.CFG.fromstring(cfg)
-for sentence in generate(grammar1):
+grammar = nltk.CFG.fromstring(cfg)
+for sentence in generate(grammar):
     print(' '.join(sentence))
-
-
-
-
-
 
